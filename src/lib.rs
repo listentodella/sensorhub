98c2c3beeaa2349f4ad805db1d2a805799c2ad7d
leo <leo233.huang@gmail.com>
2025-06-25 12:40:36 +0800

    extend SensorOps for SensorAttr

--- src/lib.rs
+++ src/lib.rs
@@ -44,9 +44,13 @@ pub enum SensorType {
 }
 
 pub trait SensorOps: Sync {
-    fn detect(&self) -> bool {
+    fn probe(&self) -> bool {
         trace!("default detect");
         false
+    }
+
+    fn remove(&self) {
+        trace!("default remove");
     }
 
     fn open(&mut self, req_odr: u32) {
@@ -66,11 +70,50 @@ pub trait SensorOps: Sync {
     fn batch(&mut self) {
         trace!("default batch");
     }
+
+    fn attrs(&self) -> &Vec<SensorAttr>;
+    fn attrs_mut(&mut self) -> &mut Vec<SensorAttr>;
+
+    /// try to get the sensor attr from the sensor
+    fn get_attr(&self, attr: SensorAttr) -> Option<&SensorAttr> {
+        self.attrs().iter().find(|&a| a.id() == attr.id())
+    }
+
+    /// try to set the sensor attr to the sensor
+    /// if the sensor already has the attr, it will be updated
+    /// if the sensor doesn't have the attr, it will be added
+    fn set_attr(&mut self, attr: SensorAttr) {
+        if let Some(old) = self.attrs_mut().iter_mut().find(|a| a.id() == attr.id()) {
+            *old = attr;
+        } else {
+            self.attrs_mut().push(attr);
+        }
+    }
+}
+
+pub struct SensorDriver {
+    pub sensor_type: SensorType,
+    pub ops: &'static dyn SensorOps,
+}
+
+collect_sensors! {SensorDriver}
+
+fn probe_all_sensors() {
+    for sensor in sensor_inventory::iter::<SensorDriver>() {
+        info!("Probing sensor: {:?}", sensor.sensor_type);
+        sensor.ops.probe();
+    }
+}
+
+pub fn init() {
+    probe_all_sensors();
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{SensorOps, SensorType, collect_sensors, register_sensor, sensor_inventory};
+    use crate::{
+        SensorAttr, SensorOps, SensorType, collect_sensors, register_sensor, sensor_inventory,
+    };
     use log::info;
     use strum::IntoEnumIterator;
 
@@ -81,40 +124,60 @@ mod tests {
         }
     }
 
-    struct SensorDriver {
+    struct SensorDriverTest {
         pub sensor_type: SensorType,
         pub ops: &'static dyn SensorOps,
     }
 
-    collect_sensors! {SensorDriver}
-    struct MySensor1;
-    impl SensorOps for MySensor1 {}
-    static MY_SENSOR1: MySensor1 = MySensor1;
-    register_sensor! {SensorDriver {
+    collect_sensors! {SensorDriverTest}
+    struct MySensor1 {
+        attrs: Vec<SensorAttr>,
+    }
+    impl SensorOps for MySensor1 {
+        fn attrs(&self) -> &Vec<SensorAttr> {
+            &self.attrs
+        }
+        fn attrs_mut(&mut self) -> &mut Vec<SensorAttr> {
+            &mut self.attrs
+        }
+    }
+    static MY_SENSOR1: MySensor1 = MySensor1 { attrs: vec![] };
+    register_sensor! {SensorDriverTest {
         sensor_type: SensorType::Accelerometer,
         ops: &MY_SENSOR1,
     }}
 
     struct MySensor2 {
         _val: u32,
+        attrs: Vec<SensorAttr>,
     }
-    impl SensorOps for MySensor2 {}
-    static MY_SENSOR2: MySensor2 = MySensor2 { _val: 233 };
-    register_sensor! {SensorDriver {
+    impl SensorOps for MySensor2 {
+        fn attrs(&self) -> &Vec<SensorAttr> {
+            &self.attrs
+        }
+        fn attrs_mut(&mut self) -> &mut Vec<SensorAttr> {
+            &mut self.attrs
+        }
+
+        fn probe(&self) -> bool {
+            info!("MySensor2 detected with value: {}", self._val);
+            true
+        }
+    }
+    static MY_SENSOR2: MySensor2 = MySensor2 {
+        _val: 233,
+        attrs: vec![],
+    };
+    register_sensor! {SensorDriverTest {
         sensor_type: SensorType::Gyroscope,
         ops: &MY_SENSOR2,
     }}
 
     #[test]
     fn probe_all_sensors() {
-        for sensor in sensor_inventory::iter::<SensorDriver>() {
+        for sensor in sensor_inventory::iter::<SensorDriverTest>() {
             info!("====>>>>Probing sensor: {:?}", sensor.sensor_type);
-            sensor.ops.detect();
+            sensor.ops.probe();
         }
     }
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct Sensor {
-    attrs: Vec<SensorAttr>,
 }
